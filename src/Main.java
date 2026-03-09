84f60a7e4da619908c0ff5ae8d334a14acc5f89c
Luciano dos Santos <lsantos.github@gmail.com>
2026-03-09 18:07:05 -0300

    Exercicio_06.java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    static void main(String[] args) {
+
+        System.out.println("Hello Word!\n" + "Hoje dou inicio uma nova jornada na minha vida\n" + "Projeto PROGRAMADOR BACK - END JAVA");
+    }
+}
